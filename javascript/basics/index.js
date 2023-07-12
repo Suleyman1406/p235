decbb4c137ce8b3e5ec1fcdc8b744382a83b3aa7
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-07-12 17:00:16 +0400

    javascript basics added

---
+++ javascript/basics/index.js
@@ -0,0 +1,197 @@
+// int a = 12;
+// string b = "test";
+// bool c = true;
+
+// let a = 12;
+// let b = "test";
+// let c = true;
+// let d = 'test';
+// let e = 'e';
+// let f = 12.4;
+
+// int a = 12;
+// a= "sad";
+
+// let a = "asd";
+// a=12;
+// console.log(a);
+
+// var a =12;
+// var b = "string;";
+
+// let a = 12;
+// let a = 13;
+
+// let $sd=  "asd";
+
+// let a;
+
+// a=12;
+
+// const a;
+// a=13;
+// console.log(a);
+
+// let for = 12;
+
+// int[] a =new int[4];
+// let a = [12, 14, 15, 18, "test", true, 12.4];
+
+// a[20]=55;
+
+// a.push("p235")
+// a.push("p517")
+// a.unshift("bitinci")
+// a.pop();
+// a.shift();
+
+// console.log(a.length);
+
+// let age = 12;
+
+// if(age<18){
+//   console.log("kicik");
+// }else if(age>18){
+//   console.log("boyuk");
+// }else{
+//   console.log("beraber");
+// }
+
+// for(let i=0; i<10;i++){
+//   if(i==2){
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// let i = 2;
+
+// while (i < 10) {
+//   console.log(i);
+//   i++;
+// }
+// let i = 2;
+
+// do {
+//   console.log(i);
+//   i++;
+// } while (i < 1);
+
+// let a = 12;
+// console.log(12 + "asd");
+
+// console.log("" + 1 + 2 + (2 + 4));
+
+// let description = "testtesttesttesttest";
+// let desc2 ='To avoid an infinity mirror, don\'t share your entire screen or browser window. Share just a tab or a different window instead.';
+
+// let desc3 = `To avoid an 
+// infinity mirror, don't share your entire screen 
+// or browser window. Share just a tab or a 
+// different window instead.`
+
+// let a =12;
+// let b =13;
+
+// console.log(`${a}-${b}`);
+
+
+// let a  = 12.5;
+
+// console.log(typeof a);
+
+// let a ="asd";
+
+// if(typeof a == "number"){
+//   console.log(a+5);
+// }else{
+//   console.log("number deyil");
+// }
+
+
+// let a = "12";
+
+// console.log(Number(a)+4);
+// console.log(Number(false));
+// console.log(+"1s2"+15);
+
+// console.log(Boolean(-5));
+// console.log(Boolean(" "));
+
+// console.log(String(5)+12);
+// console.log(""+5+12);
+
+// console.log(12+5);
+// console.log(12-5);
+// console.log(12*5);
+// console.log(12/5);
+// console.log(3%5);
+// console.log(3**5);
+
+// let a = 2;
+
+// a= a+1;
+// a+=6;
+
+// a++;
+// a++;
+// ++a;
+// ++a;
+
+
+// console.log(a);
+
+
+// let a = 1; // 1 2 3
+// let b = ++a; // 2 3
+// let c = b++ + a++; // 2 + 2 = 4
+// console.log(a); 
+// console.log(b); 
+// console.log(c); 
+
+// let a =2; // 3 4 5
+
+// console.log(`${a++} ${++a + ++a}`); // 2  9
+
+// a 3 4 3 2 1
+// b 0 -1 0 -1
+// c -2 -1
+// d 0
+
+// let a = 3;
+// let b = a++ - --a; // 3 - 3 
+// let c = b-- - --a + ++b; // 0 - 2 + 0
+// let d = c++ + a-- - b--; // -2 + 2 - 0 
+// console.log(`${a} ${b} ${c} ${d}`);
+
+
+
+
+
+// 3 1 -1 2 Nicat
+// 1 2 2 1 Elbrus
+// 1 3 4 3 Merahim
+// 1 -1 -1 0 Yunis
+// 2 6 8 4 Mahmud
+// 2 0 6 7 Ayxan
+// 1 0 0 1 Necef
+// 2 -1 -1 1 Ferid
+// 1 -1 -1 1 Murad
+
+
+
+// let a =13;
+// let b = 13;
+
+// console.log(a<=b);
+
+// let a = 13;
+// let b = 13;
+
+// console.log(13==13.0000000);
+
+
+// console.log(true && false && );
+
+// console.log("trial" && "" && "temp");
+// console.log("" || "test2");
---
+++ javascript/introduction/main.js
@@ -0,0 +1,22 @@
+// console.log("Hello World")
+// console.log("Test")
+
+/*
+  multi
+  line 
+  comment
+*/
+
+//#region 
+//  console.log("test");
+//#endregion
+
+// console.log("test");
+
+// console.loge("tesr");
+
+
+
+
+
+
