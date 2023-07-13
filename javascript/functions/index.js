bd645027066a5811cf266e648fc73e4602b8bd31
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-07-13 14:36:26 +0400

    functions added

---
+++ javascript/functions/index.js
@@ -0,0 +1,63 @@
+// function printHello(){
+//   console.log("Hello World");
+// }
+
+// printHello();
+
+// function printHelloToSomeone(name){
+//   console.log("Hello " + name)
+// }
+
+// printHelloToSomeone("Kamil");
+// printHelloToSomeone("Mahmud")
+
+// function sum(a,b){
+//   return a+b;
+// }
+// let a = sum(2,3);
+
+// console.log("cavab: ", a)
+
+// function any(p1, p2){
+//   console.log( 'p1: ', p1);
+//   console.log( 'p2: ', p2);
+// }
+
+// any(2, 3, 4, 5, 56)
+
+// function sum(a, c=55 ,b = 33 ){
+//   console.log("a", a);
+//   console.log("b", b);
+// }
+// sum(1, 5)
+
+// let sum = function(a,b){
+//   console.log(`cem: ${a+b}`);
+// }
+
+// sum(2,3)
+
+// printHello();
+
+// function printHello(){
+//   console.log("Hello world");
+// }
+
+// printHello2();
+
+// let printHello2 = function(){
+//   console.log("Hello world 2");
+// }
+
+
+// let printHello3 = () => {
+//   console.log("Print Hello 3");
+// }
+
+// printHello3();
+
+let sum = (a,b)=>{
+  return a+b;
+}
+
+console.log(sum(2,3));
